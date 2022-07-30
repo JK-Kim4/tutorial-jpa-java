6601314acb50a59353cef6df990697417e4fc467
jongwan <jongbell4@gmail.com>
2022-07-30 19:32:13 +0900

    - H2 Database 연동 - 로컬 환경에서 H2 실행 후 persistence.xml 의 설정대로 접속이 안되는 경우가 있음 - https://devlog-wjdrbs96.tistory.com/381 참고하여 H2접속 설정 완료 - h2 db 초기 설정 connection 갯수가 1개인 듯, 브라우져로 접속이 되어있다면 프로젝트에서 로직이 정상적으로 동작하지 않을 수도 있음
    Author : KJW
    Date : 2022.07.30

--- src/main/java/com/template/springjpa/JpaMain.java
+++ src/main/java/com/template/springjpa/JpaMain.java
@@ -1,10 +1,14 @@
 package com.template.springjpa;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
+@Slf4j
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -17,14 +21,43 @@ public class JpaMain {
 
         //start
         try{
+            System.out.println("logic start!");
             tx.begin();
             //.....logic
+            logic(em);
             tx.commit();
         }catch (Exception e){
+            log.error("error occur", e);
             tx.rollback();
         }finally {
             em.clear();
         }
         emf.close();
+    }
+
+    private static void logic(EntityManager em){
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("아이디");
+        member.setAge(2);
+
+        //등록
+        em.persist(member);
+
+        //수정
+        member.setAge(20);
+
+        //단건 조회
+        Member findMember = em.find(Member.class, id);
+        System.out.println("find member = " + findMember.getUsername() + ", age = " + findMember.getAge());
+
+        //목록 조회
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        System.out.println("member.size = " + members.size());
+
+        //삭제
+        em.remove(member);
     }
 }
---
+++ src/main/java/com/template/springjpa/Member.java
@@ -0,0 +1,24 @@
+package com.template.springjpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MEMBER")
+@Getter
+@Setter
+public class Member {
+    @Id
+    @Column(name ="ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String username;
+
+    private Integer age;
+}
