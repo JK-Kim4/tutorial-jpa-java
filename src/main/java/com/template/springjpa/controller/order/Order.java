61c27df098491a9742f2f0a0aeed77f818b3354e
jongwan <jongbell4@gmail.com>
2022-08-01 22:53:01 +0900

    - 1:N 관계 맵핑 시작
    Author : KJW
    Date : 2022.08.01

--- src/main/java/com/template/springjpa/controller/order/Order.java
+++ src/main/java/com/template/springjpa/controller/order/Order.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "ORDERS")
@@ -22,7 +23,7 @@ public class Order {
     private Long memberId;
 
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime orderDate; // 주문 날짜
+    private Date orderDate; // 주문 날짜
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태
