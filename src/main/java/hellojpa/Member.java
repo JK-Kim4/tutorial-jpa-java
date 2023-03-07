af582e7e9f22c75b0e484edaed6957c8976e0453
jongwan <jongbell4@gmail.com>
2023-03-07 23:29:46 +0900

    #3 엔티티 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,8 +2,10 @@ package hellojpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+@Entity(name = "Member") // Default: Class Name
+@Table(name="MBR") // 맵핑 될 database 테이블 이름 별도 설정
 public class Member {
     @Id
     private Long id;
