c8d7aeca5c04ab2c8d5f35073420e7ead686e8d8
jongwankim <jongbell4@gmail.com>
2023-03-10 12:42:39 +0900

    #3 엔티티매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -7,6 +7,7 @@ import java.util.Date;
 @Table // 맵핑 될 database 테이블 이름 별도 설정
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "name")
     private String username;
@@ -26,5 +27,29 @@ public class Member {
 
     public Member(){
 
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
