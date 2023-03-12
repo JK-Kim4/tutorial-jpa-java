bcebaed72479c0d04760a1789ad38a7bc178d9fb
jongwan <jongbell4@gmail.com>
2023-03-12 21:52:49 +0900

    #5 단방향 연관관계

--- src/main/java/com/template/springjpa/domain/Member.java
+++ src/main/java/com/template/springjpa/domain/Member.java
@@ -12,6 +12,31 @@ public class Member {
     @Column(name = "MEMBER_NAME")
     private String name;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
