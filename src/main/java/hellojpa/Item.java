07059ad8c8257625b763eb8fca717cb58e4852b6
jongwan <jongbell4@gmail.com>
2023-03-20 14:39:27 +0900

    #6 상속관계 Join 전략  - TABLE_PER_CLASS 전략에서 부모 클래스 abstract  - TABLE_PER_CLASS 전략 사용 시 select 객체를 부모 객체로 설정한 경우 불필요한 union 발생

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,7 +3,7 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // union
 public abstract class Item {
 
     @Id @GeneratedValue
