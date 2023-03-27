58fc695bd26932aa159117ef7aea35feda3f3f5e
jongwan <jw.kim@changbi.com>
2023-03-27 16:36:25 +0900

    #7 지연로딩 fetch EAGER N + 1문제 발생

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,23 +19,34 @@ public class JpaMain {
             team1.setName("team1");
             em.persist(team1);
 
+            Team team2 = new Team();
+            team2.setName("team2");
+            em.persist(team2);
+
             Member member1 = new Member();
             member1.setUsername("tester");
             member1.setTeam(team1);
             em.persist(member1);
 
+            Member member2 = new Member();
+            member2.setUsername("tester2");
+            member2.setTeam(team2);
+            em.persist(member2);
+
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member1.getId());
+            /*Member m = em.find(Member.class, member1.getId());*/
 
             //LAZY Loading => class hellojpa.Team$HibernateProxy$Zx1128IM
             //EAGER Loading => class hellojpa.Team
-            System.out.println("find member = " + m.getTeam().getClass());
+            /*System.out.println("find member = " + m.getTeam().getClass());
 
             System.out.println("=================================");
             m.getTeam().getName();
-            System.out.println("find member = " + m.getTeam().getClass());
+            System.out.println("find member = " + m.getTeam().getClass());*/
+
+            List<Member> members =  em.createQuery("select m from Member m", Member.class).getResultList();
 
 
             tx.commit();
