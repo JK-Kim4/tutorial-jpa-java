8d46d0cf39daa1d2a61cd689d983dcd25a800ad4
jongwan <jw.kim@changbi.com>
2023-03-31 17:39:25 +0900

    temp

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -46,12 +45,12 @@ public class JpaMain {
             m.getTeam().getName();
             System.out.println("find member = " + m.getTeam().getClass());*/
 
-            Child child1 = new Child();
+            /*Child child1 = new Child();
             Child child2 = new Child();
 
             Parent parent = new Parent();
             parent.addChild(child1);
-            parent.addChild(child2);
+            parent.addChild(child2);*/
 
             /*
             Cascade 설정 안했을 경우
