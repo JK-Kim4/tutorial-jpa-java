d9ab39163653995dc94dc6a9b0532cbd9fce8e54
jongwankim <jongbell4@gmail.com>
2023-06-21 17:48:49 +0900

    feat: self join

--- src/main/java/com/template/springjpa/domain/item/Item.java
+++ src/main/java/com/template/springjpa/domain/item/Item.java
@@ -1,9 +1,12 @@
 package com.template.springjpa.domain.item;
 
+import com.template.springjpa.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -18,6 +21,9 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
 
 }
