ca04fe17925d9b34108804a8e12355f913bd30d3
jongwankim <jongbell4@gmail.com>
2023-07-03 17:55:13 +0900

    feat: entity LAZY loading

--- src/main/java/com/template/springjpa/domain/Address.java
+++ src/main/java/com/template/springjpa/domain/Address.java
@@ -6,8 +6,17 @@ import lombok.Setter;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
+
+    //JPA 리플렉션, 프록시 학습 후
+    protected Address(){}
+
+    public Address(String city, String street, String zipcode){
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 
     private String city;
     private String street;
--- src/main/java/hellojpa/Child.java
+++ src/main/java/hellojpa/Child.java
@@ -10,7 +10,7 @@ public class Child {
 
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "PARENT_ID")
     private Parent parent;
 
