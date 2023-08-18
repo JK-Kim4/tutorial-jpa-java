ba643f163dcb19d435cb6983228c9c4f0cbc3b51
jongwankim <jongbell4@gmail.com>
2023-08-18 17:51:52 +0900

    feat: 사용자 페이지 및 컨트롤러 작업

--- src/main/java/com/template/springjpa/controller/HomeController.java
+++ src/main/java/com/template/springjpa/controller/HomeController.java
@@ -1,14 +1,19 @@
-package com.template.springjpa;
+package com.template.springjpa.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class HomeController {
 
     @RequestMapping("/")
-    public void indexPage(){
+    public String indexPage(){
 
+        log.info("HomeController return home");
+
+        return "home";
     }
 
 }
