5ff73952aa77b344f9063c7bd7bce5f428f7181b
jongwankim <jongbell4@gmail.com>
2023-08-21 16:17:57 +0900

    feat: javax.validation @NotEmpty, @Valid 사용하여 에러 메세지 표시 (BindingResult)

---
+++ src/main/java/com/template/springjpa/controller/MemberController.java
@@ -0,0 +1,53 @@
+package com.template.springjpa.controller;
+
+import com.template.springjpa.domain.Address;
+import com.template.springjpa.domain.Member;
+import com.template.springjpa.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberFrom", new MemberForm());
+        return "/members/createMemberForm";
+    }
+
+    /**
+     * @Valid
+     *  -> @NotNull(entity)
+     *  필수값 검증
+     *
+     *  BindingResult
+     *  -> 오류가 발생한다면 오류 정보를 BindingResult 객체에 담은 후 로직 실행
+     * */
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm memberForm, BindingResult result){
+
+        //오류 발생 시
+        if(result.hasErrors()){
+            return "/members/createMemberForm";
+        }
+
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+}
--- src/main/java/com/template/springjpa/controller/MemberForm.java
+++ src/main/java/com/template/springjpa/controller/MemberForm.java
@@ -3,6 +3,16 @@ package com.template.springjpa.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter @Setter
 public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수값 입니다.")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
 }
