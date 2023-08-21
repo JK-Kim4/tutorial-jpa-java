bbf415e9459138581c62a7849ec15bd055487d10
jongwankim <jongbell4@gmail.com>
2023-08-21 17:29:54 +0900

    feat: update form

--- src/main/java/com/template/springjpa/controller/MemberController.java
+++ src/main/java/com/template/springjpa/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,7 +22,7 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm(Model model){
         model.addAttribute("memberFrom", new MemberForm());
-        return "/members/createMemberForm";
+        return "members/createMemberForm";
     }
 
     /**
@@ -37,7 +38,7 @@ public class MemberController {
 
         //오류 발생 시
         if(result.hasErrors()){
-            return "/members/createMemberForm";
+            return "members/createMemberForm";
         }
 
         Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
@@ -48,6 +49,13 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
