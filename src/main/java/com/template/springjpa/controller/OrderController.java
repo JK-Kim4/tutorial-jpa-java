a3a08571f59e86ca1378fc65e181ce511f6ac33f
jongwankim <jongbell4@gmail.com>
2023-08-22 16:50:06 +0900

    feat: JPA 활용 1 종료

--- src/main/java/com/template/springjpa/controller/OrderController.java
+++ src/main/java/com/template/springjpa/controller/OrderController.java
@@ -9,10 +9,7 @@ import com.template.springjpa.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -54,6 +51,12 @@ public class OrderController {
         model.addAttribute("orders", orders);
 
         return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId){
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
     }
 
 
