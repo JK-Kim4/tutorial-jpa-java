70ccdbe6fd2f06b37ef298bf5bbab983c8dd8c08
jongwankim <jongbell4@gmail.com>
2023-08-22 16:22:57 +0900

    feat: 변경 감지, merge

--- src/main/java/com/template/springjpa/service/ItemService.java
+++ src/main/java/com/template/springjpa/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.template.springjpa.service;
 
+import com.template.springjpa.domain.item.Book;
 import com.template.springjpa.domain.item.Item;
 import com.template.springjpa.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,18 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public Item updateItem(Long itemId, Book param){
+        Item item = itemRepository.findOne(itemId);
+
+        //Entity 메소드를 작성하여 변경 적용하는 것이 바람직
+        item.setName(param.getName());
+        item.setPrice(param.getPrice());
+        item.setStockQuantity(param.getStockQuantity());
+
+        return item;
     }
 
     public List<Item> findItems(){
